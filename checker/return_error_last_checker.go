69ba6093f1b99cfd4b89fe8eb58c2f126dff225d
Stanislav Gatev <mail@s2gatev.com>
2017-10-27 00:04:30 +0300

    Add ReturnErrorLastChecker
    ReturnErrorLastChecker checks that error is the last value returned
    by a func.

---
+++ checker/return_error_last_checker.go
@@ -0,0 +1,57 @@
+package checker
+
+import (
+	"fmt"
+	"go/ast"
+)
+
+func init() {
+	must(Register(NewReturnErrorLastChecker))
+}
+
+// ReturnErrorLastChecker checks that error is the last value returned
+// by a func.
+type ReturnErrorLastChecker struct{}
+
+// NewReturnErrorLastChecker constructs a ReturnErrorLastChecker.
+func NewReturnErrorLastChecker() NodeChecker {
+	return &ReturnErrorLastChecker{}
+}
+
+// Slug implements the NodeChecker interface.
+func (c *ReturnErrorLastChecker) Slug() string {
+	return "return_error_last"
+}
+
+// Register implements the NodeChecker interface.
+func (c *ReturnErrorLastChecker) Register(fc *FileChecker) {
+	fc.On(&ast.FuncDecl{}, c)
+}
+
+// Check implements the NodeChecker interface.
+func (c *ReturnErrorLastChecker) Check(node ast.Node, report *Report) {
+	decl := node.(*ast.FuncDecl)
+
+	if decl.Type.Results == nil {
+		return
+	}
+
+	errorNotLast := false
+	results := decl.Type.Results.List
+	for _, result := range results[:len(results)-1] {
+		ident, ok := result.Type.(*ast.Ident)
+		if !ok {
+			continue
+		}
+
+		if ident.Name == "error" {
+			errorNotLast = true
+		}
+	}
+
+	if errorNotLast {
+		report.Errors = append(report.Errors,
+			fmt.Errorf("func '%s' should return error as the last value",
+				decl.Name.Name))
+	}
+}
---
+++ checker/return_error_last_checker_test.go
@@ -0,0 +1,79 @@
+package checker_test
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/s2gatev/lingo/checker"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReturnErrorLastChecker(t *testing.T) {
+	type test struct {
+		description string
+		input       string
+		expected    Report
+	}
+
+	tests := []test{
+		{
+			description: "error not last return",
+			input: `
+				package foo
+
+				func Foo() (int, error, bool) {}
+			`,
+			expected: Report{
+				Errors: []error{
+					fmt.Errorf("func 'Foo' should return error as the last value"),
+				},
+			},
+		},
+		{
+			description: "error last return",
+			input: `
+				package foo
+
+				func Foo() (int, bool, error) {}
+			`,
+			expected: Report{
+				Errors: nil,
+			},
+		},
+		{
+			description: "no return",
+			input: `
+				package foo
+
+				func Foo() (int, bool, error) {}
+			`,
+			expected: Report{
+				Errors: nil,
+			},
+		},
+		{
+			description: "return not ident",
+			input: `
+				package foo
+
+				func Foo() struct{} {}
+			`,
+			expected: Report{
+				Errors: nil,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			checker := NewFileChecker()
+			checker.Register(NewReturnErrorLastChecker())
+
+			file := ParseFileContent(test.input)
+			var report Report
+			checker.Check(file, &report)
+			assert.Equal(t, test.expected, report)
+		})
+	}
+}
