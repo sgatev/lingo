1f3c3778a7056d8e27b9a34a97f9b8b189e9bc0e
Stanislav Gatev <mail@s2gatev.com>
2018-04-13 15:41:45 +0300

    Extend the tests and coverage of group_param_types checker

--- checker/group_param_types_checker.go
+++ checker/group_param_types_checker.go
@@ -1,6 +1,10 @@
 package checker
 
-import "go/ast"
+import (
+	"fmt"
+	"go/ast"
+	"strings"
+)
 
 func init() {
 	must(Register("group_param_types", NewGroupParamTypesChecker))
@@ -63,6 +67,8 @@ func (c *GroupParamTypesChecker) Check(
 
 func typeName(expr ast.Expr) string {
 	switch expr := expr.(type) {
+	case *ast.BasicLit:
+		return expr.Value
 	case *ast.Ident:
 		return expr.Name
 	case *ast.InterfaceType:
@@ -84,7 +90,32 @@ func typeName(expr ast.Expr) string {
 		return result + " " + typeName(expr.Value)
 	case *ast.Ellipsis:
 		return "..." + typeName(expr.Elt)
+	case *ast.MapType:
+		return fmt.Sprintf("map[%s]%s", typeName(expr.Key), typeName(expr.Value))
+	case *ast.ArrayType:
+		return fmt.Sprintf("[%s]%s", typeName(expr.Len), typeName(expr.Elt))
+	case *ast.FuncType:
+		return funcTypeName(expr)
 	default:
 		return ""
 	}
+}
+
+func funcTypeName(funcType *ast.FuncType) string {
+	var paramTypes []string
+	for _, param := range funcType.Params.List {
+		paramTypes = append(paramTypes, typeName(param.Type))
+	}
+	params := strings.Join(paramTypes, ", ")
+
+	var resultTypes []string
+	for _, result := range funcType.Results.List {
+		resultTypes = append(resultTypes, typeName(result.Type))
+	}
+	results := strings.Join(resultTypes, ", ")
+	if len(resultTypes) > 0 {
+		results = "(" + results + ")"
+	}
+
+	return fmt.Sprintf("func(%s) %s", params, results)
 }
